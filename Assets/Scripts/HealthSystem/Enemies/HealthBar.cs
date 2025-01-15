6d6b47dc674eed36405b023f53a729a52026c510
inquernon <inquernon@gmail.com>
2025-01-15 00:54:57 -0500

    Efecto Rojo en canvas
    Efecto de sangre cuando recibe dano el pj

--- Assets/Scripts/HealthSystem/Enemies/HealthBar.cs
+++ Assets/Scripts/HealthSystem/Enemies/HealthBar.cs
@@ -30,7 +30,6 @@ public class HealthBar : MonoBehaviour
         {
             easeHealthBar.value = Mathf.Lerp(easeHealthBar.value, health, lerpSpeed);
         }
-        Debug.Log(this.name);
     }
 
     public void TakeDamage(float damage)
